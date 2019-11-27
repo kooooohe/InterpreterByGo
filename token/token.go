7b07e57f9f808a0341b501246b6b13b6771a157a
kooooohe <1211lta+github@gmail.com>
2019-11-27 17:01:36 +0900

    made string functions

--- token/token.go
+++ token/token.go
@@ -12,8 +12,9 @@ const (
 	EOF     = "EOF"
 
 	// 識別子+リテラル
-	IDENT = "IDENT" // add, foobar, x, y
-	INT   = "INT"   // 134
+	IDENT  = "IDENT" // add, foobar, x, y
+	INT    = "INT"   // 134
+	STRING = "STRING"
 
 	// 演算子
 	ASSIGN   = "="
