4dda565940cbbae2961c36eac86397bcbfdd70be
kooooohe <1211lta+github@gmail.com>
2019-12-16 16:27:03 +0900

    added a array index paraser

--- parser/parser.go
+++ parser/parser.go
@@ -18,6 +18,7 @@ var precedences = map[token.TokenType]int{
 	token.SLASH:    PRODUCT,
 	token.ASTERISK: PRODUCT,
 	token.LPAREN:   CALL,
+	token.LBRACKET: INDEX,
 }
 
 type (
@@ -69,6 +70,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerInfix(token.LT, p.parseInfixExpression)
 	p.registerInfix(token.GT, p.parseInfixExpression)
 	p.registerInfix(token.LPAREN, p.parseCallExpression)
+	p.registerInfix(token.LBRACKET, p.parseIndexExpression)
 	return p
 }
 
@@ -181,6 +183,7 @@ const (
 	PRODUCT    //*
 	PREFIX     //-X or !X
 	CALL       //myFunction(X)
+	INDEX      //array[index]
 )
 
 func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
@@ -440,4 +443,16 @@ func (p *Parser) parseExpressionList(end token.TokenType) []ast.Expression {
 		return nil
 	}
 	return list
+}
+
+func (p *Parser) parseIndexExpression(left ast.Expression) ast.Expression {
+	exp := &ast.IndexExpression{Token: p.curToken, Left: left}
+
+	p.nextToken()
+	exp.Index = p.parseExpression(LOWEST)
+
+	if !p.expectPeek(token.RBRACKET) {
+		return nil
+	}
+	return exp
 }
