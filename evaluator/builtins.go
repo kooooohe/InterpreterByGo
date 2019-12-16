aff590d4eb388338856422a98342cdd331e2edb7
kooooohe <1211lta+github@gmail.com>
2019-12-16 15:17:54 +0900

    made bulitins

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -13,7 +13,6 @@ var builtins = map[string]*object.Builtin{
 				return &object.Integer{Value: int64(len(arg.Value))}
 			default:
 				return newError("argument to `len` not supported, got %s", args[0].Type())
-
 			}
 		},
 	},
